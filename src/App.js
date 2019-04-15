1224d999e9dbf99037e272c49158f40e7a5bf82c
K750i <munkin80@gmail.com>
2019-04-15 18:30:00 +0800

    Corrected state usage for currentProfile property

--- src/App.js
+++ src/App.js
@@ -9,16 +9,19 @@ export default class App extends Component {
     super(props);
 
     if (localStorage.getItem('currentProfile')) {
-      this.currentProfile = localStorage.getItem('currentProfile');
       this.state = {
         data: JSON.parse(localStorage.getItem('appStateSource'))[
-          this.currentProfile
+          localStorage.getItem('currentProfile')
         ],
+        currentProfile: localStorage.getItem('currentProfile'),
         completionStatus: {},
       };
     } else {
-      this.currentProfile = 'default';
-      this.state = {data: JSON.parse(dataStr).default, completionStatus: {}};
+      this.state = {
+        data: JSON.parse(dataStr).default,
+        completionStatus: {},
+        currentProfile: 'default',
+      };
     }
   }
 
@@ -57,43 +60,43 @@ export default class App extends Component {
       'appStateSource',
       JSON.stringify(this.storageUpdateHelper()),
     );
-    localStorage.setItem('currentProfile', this.currentProfile);
+    localStorage.setItem('currentProfile', this.state.currentProfile);
   }
 
   storageUpdateHelper = () => {
     return {
       ...JSON.parse(localStorage.getItem('appStateSource')),
-      [this.currentProfile]: this.state.data,
+      [this.state.currentProfile]: this.state.data,
     };
   };
 
   addProfile = name => {
-    this.currentProfile = name;
     // reset state to original
-    this.setState({data: JSON.parse(dataStr).default}, () => {
-      // have to do it with callback because setState is asynchronous
-      // and may not have been called before localStorage.setItem()
-      // console.log(this.state);
-      // console.log('dataobj', dataObject['default']);
-      this.persistState();
-      localStorage.setItem('currentProfile', this.currentProfile);
-      localStorage.setItem(
-        'appStateSource',
-        JSON.stringify(this.storageUpdateHelper()),
-      );
+    this.setState(
+      {data: JSON.parse(dataStr).default, currentProfile: name},
+      () => {
+        // have to do it with callback because setState is asynchronous
+        // and may not have been called before localStorage.setItem()
+        // console.log(this.state);
+        // console.log('dataobj', dataObject['default']);
+        this.persistState();
+        localStorage.setItem('currentProfile', name);
+        localStorage.setItem(
+          'appStateSource',
+          JSON.stringify(this.storageUpdateHelper()),
+        );
 
-      this.updateTaskCounter();
-      this.forceUpdate();
-    });
+        this.updateTaskCounter();
+        this.forceUpdate();
+      },
+    );
   };
 
   changeProfile = name => {
-    this.currentProfile = name;
     this.setState(
       {
-        data: JSON.parse(localStorage.getItem('appStateSource'))[
-          this.currentProfile
-        ],
+        data: JSON.parse(localStorage.getItem('appStateSource'))[name],
+        currentProfile: name,
       },
       () => {
         this.updateTaskCounter();
@@ -115,7 +118,7 @@ export default class App extends Component {
               ? Object.keys(JSON.parse(localStorage.getItem('appStateSource')))
               : ['default']
           }
-          selectedProfile={this.currentProfile}
+          selectedProfile={this.state.currentProfile}
           addProfile={this.addProfile}
           changeProfile={this.changeProfile}
         />
